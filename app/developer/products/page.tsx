96018e480c8a86600b961a0b78a5f95822bc1ea4
Chandan Kumar <sark9tceone@gmail.com>
2025-07-03 03:55:44 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- app/developer/products/page.tsx
+++ app/developer/products/page.tsx
@@ -21,6 +21,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
