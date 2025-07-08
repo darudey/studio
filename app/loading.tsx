6fe592f7d8fe9417d9d830f31941d3753095fd49
Chandan Kumar <sark9tceone@gmail.com>
2025-07-08 09:30:38 +0000

    also for when going to another page

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,8 +4,8 @@ import { Loader2 } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="flex h-[calc(100vh-4rem)] items-center justify-center">
-      <Loader2 className="h-12 w-12 animate-spin text-primary" />
+    <div className="flex h-96 items-center justify-center">
+      <Loader2 className="h-8 w-8 animate-spin text-primary" />
     </div>
   );
 }
