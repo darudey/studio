64c9777024b2fe75b67945bdc9a09fd4913287ec
Chandan Kumar <sark9tceone@gmail.com>
2025-07-08 12:15:28 +0000

    can you change cart icon colour to button border green

--- src/components/app/Header.tsx
+++ src/components/app/Header.tsx
@@ -167,7 +167,7 @@ export default function Header() {
           {isMounted && (
             <>
               <ShoppingCartSheet>
-                <Button size="icon" className="relative rounded-full bg-white text-blue-600 hover:bg-white/90">
+                <Button size="icon" className="relative rounded-full bg-white text-green-600 hover:bg-white/90">
                   <ShoppingCart className="h-5 w-5" />
                   {cartCount > 0 && (
                      <Badge variant="destructive" className="absolute -top-2 -right-2 h-5 w-5 justify-center rounded-full p-0">{cartCount}</Badge>
